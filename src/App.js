4c68b1a3b0babab48756323c9ce1ec789755fece
Rahul Ahire <ahirerahul21@gmail.com>
2020-04-29 01:51:36 +0530

    made some final changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import './App.css'
+import PlacesAutocomplete from 'react-places-autocomplete';
+export class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { address: '' };
+  }
+ 
+  handleChange = address => {
+    this.setState({ address });
+  };
+ 
+  render() {
+    return (
+      <div className="canvas">
+      <PlacesAutocomplete
+      value={this.state.address}
+      onChange={this.handleChange}
+      onSelect={this.handleSelect}
+    >
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <div>
+          <input
+            {...getInputProps({
+              placeholder: 'Search Places ...',
+              className: 'location-search-input',
+            })}
+          />
+          <div className="autocomplete-dropdown-container">
+            {loading && <div>Loading...</div>}
+            {suggestions.map(suggestion => {
+              
+              // inline style for demonstration purpose
+              const style = suggestion.active
+                ? { backgroundColor: '#42a5f5', cursor: 'pointer' }
+                : { backgroundColor: '#ffffff', cursor: 'pointer' };
+              return (
+                <div className="input-suggestion"
+                  {...getSuggestionItemProps(suggestion, {
+                    
+                    style,
+                  })}
+                >
+                <i class="material-icons">location_on  </i> <span>{suggestion.description}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </PlacesAutocomplete>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
